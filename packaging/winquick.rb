2a2097ec25ccbffe5e58a48f0d14532693ef8f33
Tobias Carlbom <tobiascarlbom@Mac-mini.carlbomsdata.com>
2026-08-25 19:48:09 +0200

    point the formula at the v0.2.0 archive

--- packaging/winquick.rb
+++ packaging/winquick.rb
@@ -11,7 +11,7 @@ class Winquick < Formula
   license "Apache-2.0"
 
   url "https://github.com/Carlboms-Data-AB/winquick/releases/download/v0.2.0/winquick-0.2.0-darwin-arm64.tar.gz"
-  sha256 "294bddf0695d5b8a9f46a3ac787479f2f77d7b2978cded928699ce349ac13f88"
+  sha256 "87e3e31b490e8df41fc2e466488b593107213206b100981129ac2c0d3114b7f1"
 
   # Apple Silicon only: the guest is ARM64 Windows and acceleration comes from
   # Apple's Hypervisor Framework.
