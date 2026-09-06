aa7d3c56bf3e8fd32df6e68326f567166ddf3100
tobias-carlbom <tobias.carlbom@gmail.com>
2026-09-06 22:48:59 +0200

    winquick 0.4.3 archive checksum

--- packaging/winquick.rb
+++ packaging/winquick.rb
@@ -8,7 +8,7 @@ class Winquick < Formula
   desc "Run commands inside a real, disposable Windows environment"
   homepage "https://github.com/carlbomsdata/winquick"
   url "https://github.com/carlbomsdata/winquick/releases/download/v0.4.3/winquick-0.4.3-darwin-arm64.tar.gz"
-  sha256 "d9c077123d8b1cbb7da2b2023ed327ee5a32a234c9224ff9ef04e6901eaae479"
+  sha256 "b8e9ef82f201bf3c2bd2b53b116698aa400df9bb368739fceac82d1838afe35c"
   license "Apache-2.0"
 
   # Apple Silicon only: the guest is ARM64 Windows and acceleration comes from
